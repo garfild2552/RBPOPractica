eed8e279c59e96557fbe7908d8e047a669bdce5d
PRBOPractica <ruslangeymer52@gmail.com>
2024-10-21 00:47:27 +0300

    Version 0.1

---
+++ src/main/java/ru/mtuci/RPBOGRT/RpbogrtApplication.java
@@ -0,0 +1,13 @@
+package ru.mtuci.RPBOGRT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpbogrtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpbogrtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/mtuci/RPBOGRT/RpbogrtApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mtuci.RPBOGRT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpbogrtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
